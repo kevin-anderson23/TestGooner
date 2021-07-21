222ad06dff599e56abc81b98ae73649d22c860b5
kevin <kevin@LAPTOP-U8KGD1J1>
2021-07-21 20:53:38 +0530

    hasitems

--- RestAssuredLearning/src/test/java/TestRest/LearnTest.java
+++ RestAssuredLearning/src/test/java/TestRest/LearnTest.java
@@ -1,39 +1,20 @@
 package TestRest;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
-import io.restassured.http.*;
-import  static io.restassured.matcher.RestAssuredMatchers.*;
-import io.restassured.specification.*;
-import io.restassured.response.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 
 public class LearnTest {	
-	@Test
-	 public void GetWeatherDetails()
-	 {   
-	 // Specify the base URL to the RESTful web service
-	 baseURI = "https://demoqa.com/utilities/weather/city";
-	 
-	 // Get the RequestSpecification of the request that you want to sent
-	 // to the server. The server is specified by the BaseURI that we have
-	 // specified in the above step.
-	 RequestSpecification httpRequest = given();
-	 
-	 // Make a request to the server by specifying the method Type and the method URL.
-	 // This will return the Response from the server. Store the response in a variable.
-	 Response response = httpRequest.request(Method.GET, "/Hyderabad");
-	 
-	 // Now let us print the body of the message to see what response
-	 // we have recieved from the server
-	 String responseBody = response.getBody().asString();
-	 System.out.println("Response Body is =>  " + responseBody);
 
-}
 	@Test 
 public void Test02(){
-		given().get("https://demoqa.com/utilities/weather/city").
+		given().get("https://reqres.in/api/users?page=2").
 		 then().
 		 statusCode(200).
-		 log().all();
+		 body("data.id[0]",equalTo(7)).
+		 body("data.first_name",hasItems("Michael","Lindsay"));
+		 //log().all();
 
 	
 }
